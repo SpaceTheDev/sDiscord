8eb2060859b3bb28cb39ca8108f7c655fa7aa121
SpaceTheDev <contact@spacethe.dev>
2022-06-19 20:50:06 -0500

    Updated versioning

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,6 @@ game 'gta5'
 name 'sDiscord'
 author 'SpaceTheDev'
 description 'Get discord roles and realtime updates on discord members'
-version '1.0.0'
+version '2.1.0'
 
 server_script 'src/server.js'
